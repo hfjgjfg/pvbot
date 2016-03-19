8542e9e938f1eb0cc93f6c4e7d7edb7a6b07b737
hfjgjfg <mohammadhaselmehri20202020@gmail.com>
2016-03-19 22:43:04 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -55,7 +55,7 @@ _just send a photo_
 
 function is_admin(msg)-- Check if user is admin or not
   local var = false
-  local admins = {}-- put your id here
+  local admins = {168546052}-- put your id here
   for k,v in pairs(admins) do
     if msg.from.id == v then
       var = true
